d1ffc8e3c41b4462e9053edd13b0339b704a994c
MadsMorsing <madsmorsing@gmail.com>
2017-05-04 15:49:23 +0200

    initial commit test

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log('Starting application...');
+
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes.js');
+
+const argv = yargs.argv;
+
+
+console.log('***Finished running application!***')
