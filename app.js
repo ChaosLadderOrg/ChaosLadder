a03d03334edd6b264911b8c47bdbfc173bd38142
Zygis <zygis0408@gmail.com>
2017-05-25 12:02:28 +0200

    Working express for createuser, getstats and creatematches

--- app.js
+++ app.js
@@ -2,44 +2,59 @@
 // const yargs = require('yargs');
 // const argv = yargs.argv;
 
-// const express = require('express');
-// var app = express();
+const express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+
 
 const { getSummonerStats, getWeeklySummonerStats } = require('./summoner.js');
 const { createUser } = require('./user.js');
+const { createMatchList } = require('./matchmaking.js');
 
 var email = 'liko@liko.com';
 var password = '123eerr11';
 var sumName = 'hellking007';
 var accountRegion = 'euw';
 
-getWeeklySummonerStats(sumName, (stats) => {
-        console.log(stats);
-         });
+// getWeeklySummonerStats(sumName, (stats) => {
+//         console.log(stats);
+//          });
 
 // createUser(email, password, sumName, accountRegion, (confirmation) => {
 //         console.log(confirmation);
 //          });
-
-// app.get('/createuser', (req, res) => {
-//     createUser(email, password, sumName, accountRegion, (confirmation) => {
-//         console.log(confirmation);
-//         res.status(200).send('CREATE USER CALLBACK', confirmation);
-
-//     });
-// });
-
-// app.get('/getstats', (req, res) => {
-//     getWeeklySummonerStats(sumName, (stats) => {
-//         console.log(stats);
-//         res.status(200).send(stats);
-//     });
-// });
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 
-// app.listen(3000, () => {
-//     console.log('Server is up on port 3000!');
-// });
+app.post('/createuser', (req, res) => {
+        console.log(req.body);
+        createUser(req.body.email, req.body.pass, req.body.sumName, req.body.region, (confirmation) => {
+                console.log(confirmation);
+                res.status(200)//.send('CREATE USER CALLBACK', confirmation);
+
+        });
+});
+
+app.get('/getstats', (req, res) => {
+        getWeeklySummonerStats(sumName, (stats) => {
+                console.log(stats);
+                res.status(200);
+                res.json(stats);
+        });
+});
+
+app.get('/creatematches', (req, res) => {
+        createMatchList((matchlist) => {
+                console.log(matchlist);
+                res.status(200).send(matchlist);
+        });
+});
+
+
+app.listen(3000, () => {
+        console.log('Server is up on port 3000!');
+});
 
 /*
 TODO Main Functionality as modules:
